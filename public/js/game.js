d71d2444f15514e62725e39716d819a7576d3036
silver-io <javier@silver.io>
2014-11-10 23:45:14 +0000

    pass first game test

---
+++ public/js/game.js
@@ -0,0 +1,18 @@
+function Game(player1, player2) {
+  this.player1 = player1;
+  this.player2 = player2;
+}
+
+Game.prototype.beatenBy = {
+      rock: { scissors: "crushes", lizard: "crushes" },
+      paper: { rock: "covers", Spock: "disproves" },
+      scissors: { paper: "cuts", lizard: "decapitates" },
+      lizard: { Spock: "poisons", paper: "eats" },
+      Spock: { rock: "vapourises" , scissors: "smashes" },
+};
+
+Game.prototype.winner = function() {
+      if(this.beatenBy[this.player1.pick][this.player2.pick] != undefined) return this.player1;
+      if(this.beatenBy[this.player2.pick][this.player1.pick] != undefined) return this.player2;
+      if(this._isSamePick) return null;
+};
