8ecc53176f2bd12041ec14f27785b7d417c71246
silver-io <javier@silver.io>
2014-11-11 00:38:31 +0000

    completes rock tests

--- spec/rockPaperScissorsLizardSpockSpec.js
+++ spec/rockPaperScissorsLizardSpockSpec.js
@@ -9,14 +9,39 @@ describe("Rock-Paper-Scissors-Lizard-Spock", function(){
 
   describe('rules of the game', function(){
     describe('rock', function(){
+
       it('should beat scissors', function(){
+
         player1.picks('rock');
         player2.picks('scissors');
         expect(game.winner()).toBe(player1);
+
       });
 
-    });
+      it('should beat lizard', function(){
 
+        player1.picks('rock');
+        player2.picks('lizard');
+        expect(game.winner()).toBe(player1)
+      })
+
+       it('should lose to paper', function() {
+
+        player1.picks('rock');
+        player2.picks('paper');
+        expect(game.winner()).toBe(player2);
+
+      });
+
+      it('should lose to Spock', function() {
+
+        player1.picks('rock');
+        player2.picks('Spock');
+        expect(game.winner()).toBe(player2);
+
+      });
+    });
   });
+
 
 });
